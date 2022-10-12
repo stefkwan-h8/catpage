122a3f1ebd28974b57abde255a7c0be68a9133be
stefkwan-h8 <82027557+stefkwan-h8@users.noreply.github.com>
2022-10-12 10:54:48 +0700

    update

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
+// baca comments dari localstorage dan convert ke js dari text
+let comments = JSON.parse(localStorage.getItem("comments"))
+
+// kalau belum ada comments
+if (!comments){
+    comments = [] // array kosong
+    localStorage.setItem("comments", JSON.stringify(comments))
+} else {
+    // sudah ada comments kita tampilkan
+    for (c of comments) {
+        document.getElementById("post-comment").innerHTML += `<p>[${c[0]}] ${c[1]}</p>`
+    }
+}
+
 function tambahComment(){
     const d = new Date();
     let komen = document.getElementById("input-comment").value
 
     document.getElementById("post-comment").innerHTML += `<p>[${d.toString()}] ${komen}</p>`
+
+    // komentar kita simpan ke local storage
+    comments.push([d.toString(), komen])
+    localStorage.setItem("comments", JSON.stringify(comments))
 }
